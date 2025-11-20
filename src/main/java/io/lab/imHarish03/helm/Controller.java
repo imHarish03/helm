114f80c3db47688410db6f9e063150b3aa0535a9
harishhari302 <harishhari302@gmail.com>
2025-11-20 15:22:09 +0530

    feat: inject env-specific properties into app using Helm values.yaml

--- src/main/java/io/lab/imHarish03/helm/Controller.java
+++ src/main/java/io/lab/imHarish03/helm/Controller.java
@@ -1,4 +1,5 @@
 package io.lab.imHarish03.helm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/helm")
 public class Controller {
 
+    @Value("${app.message}")
+    private String message;
+
     @GetMapping
-    public String getData(){
-        return "Welcome to Helm";
+    public String getData() {
+        return "Welcome to Helm: " + message;
     }
 
 }
